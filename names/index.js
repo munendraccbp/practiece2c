ac0040963a2393ea95caf99e59a28478dc6387cb
munendraccbp <r170394@rguktrkv.ac.in>
2023-02-03 14:00:37 +0000

    commit message

---
+++ names/index.js
@@ -0,0 +1,7 @@
+let nameslist = require("../country/state/city/index");
+let function = require("../utilities/utils/index");
+
+let getPeopleInCity=(nameslist)=>{
+    return function(namesList)
+}
+module.exports=getPeopleInCity;
